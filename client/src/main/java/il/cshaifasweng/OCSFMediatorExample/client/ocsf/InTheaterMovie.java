54fa56a51990cefcbef585bcee8fd22f71835d29
Amit <amitp3171@gmail.com>
2024-06-29 19:09:39 +0300

    push without errors

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/ocsf/InTheaterMovie.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/ocsf/InTheaterMovie.java
@@ -9,6 +9,6 @@ public class InTheaterMovie extends AbstractMovie{
     public InTheaterMovie(String movieName, String producerName, List<String> mainActors, String description, String picture, List<ScreeningTime> screenings) {
         super(movieName, producerName, mainActors, description, picture);
         this.days = new List[7];
-        this.screenings = new HashMap<>;
+        //this.screenings = new HashMap<>;
     }
 }
